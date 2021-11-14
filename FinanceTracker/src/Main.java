a584d0ee2d2135ae8b11af8ae3e1cc3c0e1a1e31
Anders <gillilanda@rsd407.org>
2021-11-14 09:32:06 -0500

    Added FinanceTracker project

---
+++ FinanceTracker/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * <b>Finance Tracker</b>
+ * <div>My own way of tracking the flow of my finances while in college.</div>
+ *
+ * @author Anders Gilliland
+ *
+ * @version November 14, 2021
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
